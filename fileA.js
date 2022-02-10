5856b17744c1344259bd64149718d41b6e46d1d3
Jose Silva <mcvicksilvaa@gmail.com>
2022-02-10 09:06:49 -0500

    important reflection

--- fileA.js
+++ fileA.js
@@ -2,3 +2,4 @@ print("Owon't")
 // This a a cringe mod lmao get fucked 
 // This should not be legal
 print("Cringe master mind")
+print("why not making a random billionarie to do a course in ur shitty platform")
